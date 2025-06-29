7d29072516c0d3410dd492d1353065be8d8f810b
budabalint <buda.balint@students.jedlik.eu>
2025-06-29 21:07:58 +0200

    color recognition final version

--- Camera/color _recognition.py
+++ Camera/color _recognition.py
@@ -15,25 +15,22 @@ while True:
 
     #pixel_center = hsv_frame[center_y, center_x]
     # h = pixel_center[0]
-    
-    # 5x5-ös régió
     region = hsv_frame[center_y-2:center_y+3, center_x-2:center_x+3]
     h_values = region[:, :, 0].flatten()
     
     
 
-    # Leggyakoribb H érték meghatározása numpy-val
     unique, counts = np.unique(h_values, return_counts=True)
-    mode_h = unique[np.argmax(counts)]  # ahol a legtöbbször fordul elő
+    mode_h = unique[np.argmax(counts)]
 
     color = "Unknown"
     if mode_h < 5 or mode_h > 165:
         color = "Red"
     elif 6 <= mode_h < 33:
         color = "Yellow"
-    elif 33 <= mode_h < 83:
+    elif 33 <= mode_h < 96:
         color = "Green"
-    elif 84 <= mode_h < 131:
+    elif 96 <= mode_h < 131:
         color = "Blue"
     elif 131 <= mode_h < 164:
         color = "Purple"
