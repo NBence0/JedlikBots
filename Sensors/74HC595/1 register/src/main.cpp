52c25d9bf5d4d8c7c4633359d5f74619599da2c2
NBence0 <106099342+NBence0@users.noreply.github.com>
2025-07-10 15:32:25 +0200

    Two shift registers together

--- Sensors/74HC595/1 register/src/main.cpp
+++ Sensors/74HC595/1 register/src/main.cpp
@@ -1,54 +1,102 @@
+//**************************************************************//
+//  Name    : shiftOutCode, Predefined Dual Array Style         //
+//  Author  : Carlyn Maw, Tom Igoe                              //
+//  Date    : 25 Oct, 2006                                      //
+//  Version : 1.0                                               //
+//  Notes   : Code for using a 74HC595 Shift Register           //
+//          : to count from 0 to 255                            //
 
 #include <Arduino.h>
-/*
-Shift Register Example
-for 74HC595 shift register
-This sketch turns reads serial input and uses it to set the pins
-of a 74HC595 shift register.
-Hardware:
-* 74HC595 shift register attached to pins 8, 12, and 11 of the Arduino,
-as detailed below.
-* LEDs attached to each of the outputs of the shift register.
-Created 22 May 2009
-Created 23 Mar 2010
-by Tom Igoe
-*/
-// Pin connected to latch pin (ST_CP) of 74HC595
-const int latchPin = 10;
-// Pin connected to clock pin (SH_CP) of 74HC595
-const int clockPin = 12;
-////Pin connected to Data in (DS) of 74HC595
-const int dataPin = 11;
 
+int latchPin = 10;
+int clockPin = 12;
+int dataPin = 11;
+byte dataRED;
+byte dataGREEN;
+byte dataArrayRED[10];
+byte dataArrayGREEN[10];
 
+void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
+int i=0;
+int pinState;
+pinMode(myClockPin, OUTPUT);
+pinMode(myDataPin, OUTPUT);
+digitalWrite(myDataPin, 0);
+digitalWrite(myClockPin, 0);
+for (i=7; i>=0; i--)  {
+digitalWrite(myClockPin, 0);
 
-//**************************************************************//
-//  Name    : shiftOutCode, Hello World
-//  Author  : Carlyn Maw,Tom Igoe, David A. Mellis
-//  Date    : 25 Oct, 2006
-//  Modified: 23 Mar 2010
-//  Version : 2.0
-//  Notes   : Code for using a 74HC595 Shift Register           //
-//          : to count from 0 to 255
-//****************************************************************
-void setup() {
-//set pins to output so you can control the shift register
-pinMode(latchPin, OUTPUT);
-pinMode(clockPin, OUTPUT);
-pinMode(dataPin, OUTPUT);
+if ( myDataOut & (1<<i) ) {
+pinState= 1;
 }
+else {
+pinState= 0;
+}
+
+digitalWrite(myDataPin, pinState);
+digitalWrite(myClockPin, 1);
+digitalWrite(myDataPin, 0);
+}
+digitalWrite(myClockPin, 0);
+}
+
+void blinkAll_2Bytes(int n, int d) {
+digitalWrite(latchPin, 0);
+shiftOut(dataPin, clockPin, 0);
+shiftOut(dataPin, clockPin, 0);
+digitalWrite(latchPin, 1);
+delay(200);
+for (int x = 0; x < n; x++) {
+digitalWrite(latchPin, 0);
+shiftOut(dataPin, clockPin, 255);
+shiftOut(dataPin, clockPin, 255);
+digitalWrite(latchPin, 1);
+delay(d);
+digitalWrite(latchPin, 0);
+shiftOut(dataPin, clockPin, 0);
+shiftOut(dataPin, clockPin, 0);
+digitalWrite(latchPin, 1);
+delay(d);
+}
+}
+
+
+void setup() {
+pinMode(latchPin, OUTPUT);
+Serial.begin(9600);
+dataArrayRED[0] = 0xFF; //11111111
+dataArrayRED[1] = 0xFE; //11111110
+dataArrayRED[2] = 0xFC; //11111100
+dataArrayRED[3] = 0xF8; //11111000
+dataArrayRED[4] = 0xF0; //11110000
+dataArrayRED[5] = 0xE0; //11100000
+dataArrayRED[6] = 0xC0; //11000000
+dataArrayRED[7] = 0x80; //10000000
+dataArrayRED[8] = 0x00; //00000000
+dataArrayRED[9] = 0xE0; //11100000
+
+
+dataArrayGREEN[0] = 0xFF; //11111111
+dataArrayGREEN[1] = 0x7F; //01111111
+dataArrayGREEN[2] = 0x3F; //00111111
+dataArrayGREEN[3] = 0x1F; //00011111
+dataArrayGREEN[4] = 0x0F; //00001111
+dataArrayGREEN[5] = 0x07; //00000111
+dataArrayGREEN[6] = 0x03; //00000011
+dataArrayGREEN[7] = 0x01; //00000001
+dataArrayGREEN[8] = 0x00; //00000000
+dataArrayGREEN[9] = 0x07; //00000111
+blinkAll_2Bytes(2,200);
+}
+
 void loop() {
-// count from 0 to 255 and display the number
-// on the LEDs
-for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
-// take the latchPin low so
-// the LEDs don't change while you're sending in bits:
-digitalWrite(latchPin, LOW);
-// shift out the bits:
-shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);
-//take the latch pin high so the LEDs will light up:
-digitalWrite(latchPin, HIGH);
-// pause before next value:
-delay(500);
+for (int j = 0; j < 10; j++) {
+dataRED = dataArrayRED[j];
+dataGREEN = dataArrayGREEN[j];
+digitalWrite(latchPin, 0);
+shiftOut(dataPin, clockPin, dataGREEN);
+shiftOut(dataPin, clockPin, dataRED);
+digitalWrite(latchPin, 1);
+delay(50); // ennyi időnként küld adatot
 }
 }
