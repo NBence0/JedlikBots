36bc7ccd7f10e3acc0bbce8045964c5b26b1a970
budabalint <buda.balint@students.jedlik.eu>
2025-08-28 16:45:02 +0200

    TMC full SPI mode

--- Motor/TMC5160/src/main.cpp
+++ Motor/TMC5160/src/main.cpp
@@ -1,55 +1,62 @@
 #include <TMCStepper.h>
-#include <SPI.h>
 
-// A bekötésnek megfelelő pinek
+// SPI pinek
 #define EN_PIN      33
 #define CS_PIN      39
 #define MOSI_PIN    35
 #define MISO_PIN    37
 #define SCK_PIN     36
 
-TMC5160Stepper driver = TMC5160Stepper(CS_PIN);
+#define R_SENSE 0.075f  
+
+TMC5160Stepper driver = TMC5160Stepper(CS_PIN, R_SENSE, MOSI_PIN, MISO_PIN, SCK_PIN);
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) {}
-  Serial.println("Kommunikációs teszt indítása...");
+  while (!Serial);
 
   pinMode(EN_PIN, OUTPUT);
-  digitalWrite(EN_PIN, HIGH); // Driver letiltva a biztonság kedvéért
-
-  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+  digitalWrite(EN_PIN, HIGH); // driver tiltva induláskor
 
   driver.begin();
+  driver.toff(4);
+  driver.blank_time(24);
+  driver.rms_current(800);
+  driver.microsteps(256); 
+  driver.en_pwm_mode(true);
+  driver.pwm_autoscale(true);
 
-  // Teszteljük a kapcsolatot. Ennek 0-t kell visszaadnia siker esetén.
-  uint8_t connection_result = driver.test_connection();
+  // Velocity mode
+  driver.RAMPMODE(1);        // 1 = előre velocity mode
+  driver.VMAX(50000);        // sebesség (pozitív = előre, RAMPMODE=2 esetén hátra)
 
-  Serial.print("Kommunikációs teszt eredménye: ");
-  if (connection_result == 0) {
-    Serial.println("OK! A driver válaszol.");
-  } else {
-    Serial.print("HIBA! Kód: ");
-    Serial.println(connection_result);
-    Serial.println("Ellenőrizd a bekötést (MOSI/MISO, CS, GND)!");
-    while(true) delay(100); // Hiba esetén álljon meg itt
-  }
-
-  // Olvassuk ki a GCONF (általános konfigurációs) regisztert
-  // Ha a kommunikáció működik, itt egy nem-nulla értéket kell látnunk.
-  uint32_t gconf_val = driver.GCONF();
-  Serial.print("GCONF regiszter értéke: ");
-  Serial.println(gconf_val, HEX); // Kiírjuk hexadecimálisan
-
-  // Olvassuk ki a verzió számot
-  uint8_t version = driver.IOIN();
-  Serial.print("Driver verzió (IOIN): ");
-  Serial.println(version, HEX);
-
-
-  Serial.println("Teszt vége.");
+  digitalWrite(EN_PIN, LOW); // driver engedélyezése
+  Serial.println("TMC5160 velocity mode indul!");
 }
 
 void loop() {
-  // A teszthez a loop üres
+  static uint32_t last_time = 0;
+  static bool dir = true;
+
+  if (millis() - last_time > 5000) {
+    last_time = millis();
+    dir = !dir;
+    if (dir) {
+      driver.RAMPMODE(1);   // előre
+      driver.VMAX(60000);
+      Serial.println("Előre forog");
+    } else {
+      driver.RAMPMODE(2);   // hátra
+      driver.VMAX(60000);
+      Serial.println("Hátra forog");
+    }
+  }
+
+  // Debug kiírás
+  static uint32_t last_print = 0;
+  if (millis() - last_print > 1000) {
+    last_print = millis();
+    Serial.print("XACTUAL = ");
+    Serial.println(driver.XACTUAL());
+  }
 }
